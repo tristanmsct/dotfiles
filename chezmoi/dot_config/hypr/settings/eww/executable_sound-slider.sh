ea3f41f0cd3fe36db7f15600a380430396f8968c
Tristan Muscat <tristan.muscat@gmail.com>
2025-06-01 14:15:04 +0200

    Improve eww sound slider

--- chezmoi/dot_config/hypr/settings/eww/executable_sound-slider.sh
+++ chezmoi/dot_config/hypr/settings/eww/executable_sound-slider.sh
@@ -2,5 +2,10 @@
 
 monitor_id=$(hyprctl activeworkspace -j | jq '.monitorID')
 
-eww open sound-slider-window-closer --screen $monitor_id
-eww open sound-slider-window --screen $monitor_id
+if [ $monitor_id -eq 0 ]; then
+    eww open sound-slider-window-closer --screen $monitor_id
+    eww open sound-slider-window --screen $monitor_id
+else
+    eww open sound-slider-window-secondary-closer --screen $monitor_id
+    eww open sound-slider-window-secondary --screen $monitor_id
+fi
