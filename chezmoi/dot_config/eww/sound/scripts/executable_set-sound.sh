008ff48142c5b565eba82f85f5595ba7cd54682d
Tristan Muscat <tristan.muscat@gmail.com>
2025-07-21 21:51:59 +0200

    Removed useless file write for sound icon

--- chezmoi/dot_config/eww/sound/scripts/executable_set-sound.sh
+++ chezmoi/dot_config/eww/sound/scripts/executable_set-sound.sh
@@ -13,30 +13,12 @@ volume=$(echo "$output" | grep -oP '\[\K\d+%')
 volume=$(echo $volume | sed 's/%//g')
 status=$(echo "$output" | grep -oP '\[on\]|\[off\]' | tr -d '[]')
 
-# TODO : why even write to tmp ?
-set_icon () {
-    if [ $1 = "off" ]; then
-        echo " " > /tmp/eww_sound_icon
-    else
-        if [ $2 -ge 50 ]; then
-            echo " " > /tmp/eww_sound_icon
-        elif [ $2 -gt 0 ]; then
-            echo " " > /tmp/eww_sound_icon
-        else
-            echo " " > /tmp/eww_sound_icon
-        fi
-    fi
-}
-
 if [ $new_value = "mute" ]; then
     if [ $status = "off" ]; then
         amixer set -q Master on
-        set_icon on $volume
     else
         amixer set -q Master off
-        set_icon off $volume
     fi
 else
     amixer set -q Master "${new_value}%"
-    set_icon $status $new_value
 fi
