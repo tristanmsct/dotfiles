4c179ed078470b117f093277e58da0f4bbc0f654
Tristan Muscat <tristan.muscat@gmail.com>
2025-11-10 14:09:56 +0100

    Minor changes
    - New alias for bash with .bashrc included
    - Clean up on boot of brave crash reports

--- chezmoi/dot_config/hypr/scripts/system/executable_home-cleaner.sh
+++ chezmoi/dot_config/hypr/scripts/system/executable_home-cleaner.sh
@@ -8,3 +8,7 @@
 # -----------------------------------------------------------------------------------------------------------------------------------------
 
 [ -f ~/.cache/monitor-config-running ] && rm ~/.cache/monitor-config-running
+
+# Remove Brave crash reports from home, where they do not belong.
+find "$HOME/.config/BraveSoftware/Brave-Browser/Crash Reports/completed" -name "*.dmp" -delete
+find "$HOME/.config/BraveSoftware/Brave-Browser/Crash Reports/completed" -name "*.meta" -delete
