5c921c13d13e6566e66644edb01ff7f4790c2125
Tristan Muscat <tristan.muscat@pm.me>
2026-01-17 14:14:08 +0100

    Removes simple store for brave

--- chezmoi/dot_config/hypr/launchers/executable_browser.sh
+++ chezmoi/dot_config/hypr/launchers/executable_browser.sh
@@ -3,9 +3,9 @@
 # Get XDG variables and create the fake home if it does not exists.
 source $HOME/.config/hypr/launchers/setup-fake-home.sh
 
-HOME="$FAKE_HOME" exec /usr/bin/brave --user-data-dir="$XDG_DATA_HOME/brave-data" \
+HOME="$FAKE_HOME" exec /usr/bin/brave \
+                  --user-data-dir="$XDG_DATA_HOME/brave-data" \
                   --disk-cache-dir="$XDG_CACHE_HOME/brave-cache" \
                   --enable-features=UseOzonePlatform \
                   --ozone-platform=wayland \
-                  --password-store=basic \
                   "$@"
