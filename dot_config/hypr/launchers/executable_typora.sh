ba0958dfc97f099122aa3b74a086f63ada017e86
Tristan Muscat <tristan.muscat@pm.me>
2026-03-25 10:29:52 +0100

    Add recommended flags for typora + ssh style for shell config

--- dot_config/hypr/launchers/executable_typora.sh
+++ dot_config/hypr/launchers/executable_typora.sh
@@ -3,4 +3,9 @@
 # Get XDG variables and create the fake home if it does not exists.
 source $HOME/.config/hypr/launchers/setup-fake-home.sh
 
-HOME="$FAKE_HOME" exec /usr/bin/typora --user-data-dir="$XDG_DATA_HOME/typora-data" "$@"
+HOME="$FAKE_HOME" exec /usr/bin/typora \
+                --enable-features=UseOzonePlatform \
+                --ozone-platform=wayland \
+                --enable-wayland-ime \
+                --wayland-text-input-version=3 \
+                --user-data-dir="$XDG_DATA_HOME/typora-data" "$@"
